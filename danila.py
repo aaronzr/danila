b593f4776cdc52c7f501256705ecf8205e192396
Aaron Reed <aaron73@stanford.edu>
2021-12-27 22:04:58 -0600

    change name

---
+++ danila.py
@@ -0,0 +1,21 @@
+import sys
+import subprocess
+
+# parser = argparse.ArgumentParser(description='Run danila-miner.')
+
+args = sys.argv[1:] 
+
+danila_args_idx = -1
+
+for a in args:
+    if not a[0].isupper():
+        danila_args_idx = args.index(a)
+        break
+
+print("OCMonitor args:")
+print(args[:danila_args_idx])
+
+print("Echoing other args:")
+subprocess.run(["./danila-miner"] + args[danila_args_idx:])
+
+
